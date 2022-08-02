f31366a0f41eb7a0daa0c9cee9a58d13e03a0daa
RayhungKao <ryamkao2339@gmail.com>
2022-08-02 21:58:32 +0800

    fix account db schema

--- app/db/migrations/001_accounts_create.rb
+++ app/db/migrations/001_accounts_create.rb
@@ -8,7 +8,7 @@ Sequel.migration do
       primary_key :id
 
       String :username, null: false, unique: true
-      String :email, null: false, unique: true
+      String :email, unique: true
       String :password_digest
       DateTime :created_at
       DateTime :updated_at
